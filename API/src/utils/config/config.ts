105d8f67fddc61af7cd28883f1db3a62d245c4c7
lschacon1988 <lschacon1988@gmail.com>
2023-06-05 10:50:04 -0500

    Update config.ts

--- API/src/utils/config/config.ts
+++ API/src/utils/config/config.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log(process.env.PASSWORD_DB)
+
 export default{
     jwtScret: process.env.JWT_SCRET || 'screttoken',
     DB:{
-        URI:  'mongodb+srv://lschacon1988:Luis1988@cluster0.qqekufj.mongodb.net/',
+        URI:  `mongodb+srv://lschacon1988:${process.env.PASSWORD_DB}@cluster0.qqekufj.mongodb.net/`,
         USER: process.env.MONGODB_USER,
         PASSWORD: process.env.MONGODB_PASSWORD
 
