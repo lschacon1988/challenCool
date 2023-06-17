1157bccc59306f9a8be87c2a21e7672b15bace67
lschacon1988 <lschacon1988@gmail.com>
2023-06-17 09:01:16 -0500

    update vercel

--- API/dist/utils/error/messageError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.messageError = void 0;
-const messageError = (error) => {
-    return error.message;
-};
-exports.messageError = messageError;
